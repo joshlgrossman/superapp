34944269985aed44992e3d92f169853385282e36
joshlgrossman <josh.l.grossman@gmail.com>
2017-09-19 20:29:30 -0500

    ajax functions

---
+++ src/http.js
@@ -0,0 +1,28 @@
+function http(url, method, params, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      if(xhr.status === 200) {
+        try { cb(null, JSON.parse(xhr.responseText)); }
+        catch (e) { cb(e, null); }
+      } else cb(xhr.status, null);
+    }
+  };
+  xhr.send(JSON.stringify(params));
+}
+
+export const post = (url, params, cb) => http(url, 'POST', params, cb);
+export const put = (url, params, cb) => http(url, 'PUT', params, cb);
+export const del = (url, params, cb) => http(url, 'DELETE', params, cb);
+export const get = (url, params, cb) => {
+  const paramArray = [];
+  for(const key in params) {
+    const encodedKey = encodeURIComponent(key);
+    const encodedVal = encodeURIComponent(params[key]);
+    paramArray.push(`${encodedKey}=${encodedVal}`);
+  }
+  url += `?${paramArray.join('&')}`;
+  http(url, 'GET', {}, cb);
+};
