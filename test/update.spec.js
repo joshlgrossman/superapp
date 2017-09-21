bc45a5a88f5894f599d918b2d3f218322114753c
joshlgrossman <josh.l.grossman@gmail.com>
2017-09-21 18:22:04 -0500

    init commit for update unit tests

---
+++ test/update.spec.js
@@ -0,0 +1,12 @@
+import { describe, it, beforeEach } from 'mocha';
+import chai from 'chai';
+import sinon from 'sinon-chai';
+import { expect } from 'chai';
+import { stub, match } from 'sinon';
+import { update } from '../src/update';
+
+chai.use(sinon);
+
+describe('update', () => {
+
+});
