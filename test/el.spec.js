e263c32fb689487a482a446cbb41de3660528ab3
joshlgrossman <josh.l.grossman@gmail.com>
2017-09-20 20:31:34 -0500

    refine el unit test

--- test/el.spec.js
+++ test/el.spec.js
@@ -2,38 +2,42 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { el } from '../src/el';
 
-describe('when creating an element', () => {
+describe('el', () => {
 
-  it('should create a properly structured vdom element', () => {
+  describe('when creating an element', () => {
 
-    const element = el('div', {class: 'test'},
-      el('p', {}, 'hello world'),
-      el('div', {style: 'color: red'},
-        el('button', {}, 'test')
-      )
-    );
+    it('should create a properly structured VDOM model', () => {
 
-    expect(element).to.deep.equal({
-      tag: 'div',
-      attributes: {class: 'test'},
-      children: [
-        {
-          tag: 'p',
-          attributes: {},
-          children: ['hello world']
-        },
-        {
-          tag: 'div',
-          attributes: {style: 'color: red'},
-          children: [
-            {
-              tag: 'button',
-              attributes: {},
-              children: ['test']
-            }
-          ]
-        }
-      ]
+      const element = el('div', {class: 'test'},
+        el('p', {}, 'hello world'),
+        el('div', {style: 'color: red'},
+          el('button', {}, 'test')
+        )
+      );
+
+      expect(element).to.deep.equal({
+        tag: 'div',
+        attributes: {class: 'test'},
+        children: [
+          {
+            tag: 'p',
+            attributes: {},
+            children: ['hello world']
+          },
+          {
+            tag: 'div',
+            attributes: {style: 'color: red'},
+            children: [
+              {
+                tag: 'button',
+                attributes: {},
+                children: ['test']
+              }
+            ]
+          }
+        ]
+      });
+
     });
 
   });
